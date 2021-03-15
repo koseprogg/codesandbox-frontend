c5ca4447d9640c60aae46bf083f881f0531cee0d
asmundh <aasmund.haugse@gmail.com>
2021-03-15 09:28:11 +0100

    parseInt on score in Nutpage

--- src/components/Nutpage.tsx
+++ src/components/Nutpage.tsx
@@ -38,7 +38,7 @@ const Nutpage: React.FC = () => {
     });
     console.log(response);
     setResponse(JSON.stringify(response.data.result));
-    setScore(JSON.stringify(response.data.msg));
+    setScore(Number.parseInt(JSON.stringify(response.data.msg), 10));
   }
 
   return (
