2d19e6ef2695faab4391714b584fcba943d5e207
Mithunan Sivakumar <mithunan2522@gmail.com>
2021-03-18 22:29:01 +0100

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Mainpage from "./components/Mainpage";
 import Nutpage from "./components/Nutpage"
-import Signup from "./Signup"
+import Signup from "./components/Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Dashboard from "./Dashboard"
 import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
+import PrivateRoute from "./components/PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
 import UpdateProfile from "./UpdateProfile"
 import MyProfile from "./MyProfile"
