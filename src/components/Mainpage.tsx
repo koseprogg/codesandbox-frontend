a2911efc85b2c359b75c3ed1be1388fdce26a96d
Sindre Sørensen <>
2021-03-22 13:46:42 +0100

    Did some reformatting. Added proper msg when no competitions present.

--- src/components/Mainpage.tsx
+++ src/components/Mainpage.tsx
@@ -12,21 +12,21 @@ const abakusLogo = require('../../public/images/abakus_logo_improved.png').defau
 const backendUrl = 'http://localhost:3000';
 
 const competitions = [
-  { 
-      name: "Påskenøtter 2021", 
-      image: "https://thumbs.dreamstime.com/b/toy-easter-chickne-2102239.jpg",
-      isActive: true
+  {
+    name: "Påskenøtter 2021",
+    image: "https://thumbs.dreamstime.com/b/toy-easter-chickne-2102239.jpg",
+    isActive: true
   },
   {
-      name: "Julenøtter 2021", 
-      image: "https://image.forskning.no/122244.jpg?imageId=122244&width=480&height=274",
-      isActive: true
+    name: "Julenøtter 2021",
+    image: "https://image.forskning.no/122244.jpg?imageId=122244&width=480&height=274",
+    isActive: true
   },
   {
-    name: "Ballenøtter 2021", 
+    name: "Ballenøtter 2021",
     image: "https://lyngstadernaering.no/wp-content/uploads/2014/06/breads_flavored_with_onion_and_garlic_2.jpg",
     isActive: true
-}
+  }
 ];
 
 const Mainpage: React.FC = () => {
@@ -38,6 +38,7 @@ const Mainpage: React.FC = () => {
     if (response != null && !error) {
       setCompetitions(response);
     }
+    console.log("competitions is: ", competitions);
   })
 
   return (
@@ -47,15 +48,19 @@ const Mainpage: React.FC = () => {
         <img height="50" src={abakusLogo} alt={'abakuslogo'} />
       </div>
       <div className="competition-container">
-        {competitions && competitions.map((competition, i: number) => {
-            return <ImageCard 
-                      key={i}
-                      name={competition.name} 
-                      image={competition.image} 
-                      isTask={false}
-                      // isActive={competition.isActive} 
-                    />
-          })}
+        {competitions.length > 0 ? competitions.map((competition, i: number) => {
+          return <ImageCard
+            key={i}
+            name={competition.name}
+            image={competition.image}
+            isTask={false}
+          // isActive={competition.isActive} 
+          />
+        })
+          :
+          <p>
+            No competition to show. Backend server might be down.
+          </p>}
       </div>
     </div>
   );
