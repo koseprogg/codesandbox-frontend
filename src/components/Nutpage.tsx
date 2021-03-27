ba27ae27818434cb760a4e964a45046d96cfcecf
Ludvig Hozman <ludvig123@gmail.com>
2021-03-27 19:02:36 +0100

    Make the fetch url dynamic

--- src/components/Nutpage.tsx
+++ src/components/Nutpage.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from "react";
 import "./Nutpage.css";
 import axios from "axios";
@@ -8,12 +7,12 @@ import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/theme/neat.css";
 import "codemirror/mode/javascript/javascript.js";
-import { Task } from '../shared/types';
+import { Task } from "../shared/types";
 import { useFetch } from "../hooks/useFetch";
 import { Alert, Button } from "react-bootstrap";
 import { useRouteMatch } from "react-router-dom";
 
-const backendUrl = 'http://localhost:3000'
+const backendUrl = "http://localhost:3000";
 
 interface MatchParams {
   name: string;
@@ -22,16 +21,17 @@ interface MatchParams {
 
 const Nutpage: React.FC = () => {
   const [task, setTask] = useState<Task>();
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
   const [score, setScore] = useState<number>();
   const [errorMsg, setErrorMsg] = useState("");
 
-  const match = useRouteMatch<MatchParams>('/:name/day/:day');
+  const match = useRouteMatch<MatchParams>("/:name/day/:day");
 
   const { response, error } = match
-    ? useFetch(`${backendUrl}/competitions/${match.params.name}/day/${match.params.day}`)
+    ? useFetch(
+        `${backendUrl}/competitions/${match.params.name}/day/${match.params.day}`
+      )
     : null;
-
 
   React.useEffect(() => {
     if (response != null && !error) {
@@ -46,25 +46,32 @@ const Nutpage: React.FC = () => {
   }
 
   async function sendCode() {
-    const url = "http://localhost:3000/competitions/nuts/Påskenøtt/6";
+    if (!match?.params.name || !match.params.day) {
+      setErrorMsg("Could not parse competition name and/or day");
+      return;
+    }
+    const url = `${backendUrl}/competitions/${match.params.name}/day/${match.params.day}`;
     const response = await axios.post(url, {
       code: code,
     });
     console.log(response);
-    setScore(Number.parseInt(JSON.stringify(response.data.msg), 10));
-    setErrorMsg(JSON.stringify(response.data.msg));
+    if (response.status != 200) {
+      setErrorMsg(JSON.stringify(response.data?.msg));
+      return;
+    }
+    setScore(Number.parseInt(JSON.stringify(response.data?.msg), 10));
   }
 
   const displayErrorMessage = () => {
-    return errorMsg !== "" ? 
-    ( <Alert variant="danger">
-        Error message: {errorMsg} 
-      </Alert>
-    ) : <React.Fragment />
-  }
+    return errorMsg !== "" ? (
+      <Alert variant="danger">Error message: {errorMsg}</Alert>
+    ) : (
+      <React.Fragment />
+    );
+  };
 
   console.log(task);
-  
+
   return task && match ? (
     <div>
       <h1 className="main-heading">{`Dag ${match.params.day}: ${task.name}`}</h1>
@@ -89,23 +96,26 @@ const Nutpage: React.FC = () => {
           <span className="task-description">{task.description}</span>
           <div className="subtask-container">
             <li>
-              {task.subtasks.map((subtask: string, i: number) => <ul key={i}>{subtask}</ul>)}
+              {task.subtasks.map((subtask: string, i: number) => (
+                <ul key={i}>{subtask}</ul>
+              ))}
             </li>
           </div>
         </div>
       </div>
       <div className="codeMirror-editor">
-        
-        {score && <Alert variant="primary">
-          {`Score: ${score}%`}
-        </Alert>}
+        {score && <Alert variant="primary">{`Score: ${score}%`}</Alert>}
         {displayErrorMessage()}
-        <Button onClick={sendCode} variant="primary">Send kode</Button>  
+        <Button onClick={sendCode} variant="primary">
+          Send kode
+        </Button>
       </div>
     </div>
-  ) : <div>
-        <p>Laster ...</p>
-      </div>;
+  ) : (
+    <div>
+      <p>Laster ...</p>
+    </div>
+  );
 };
 
 export default Nutpage;
