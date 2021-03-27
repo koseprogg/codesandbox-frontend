1ad80dcf65abec6b846611b6210fb3c598999ffe
Ludvig Hozman <ludvig123@gmail.com>
2021-03-27 22:47:51 +0100

    Add auth to fetch & example login button

--- src/components/Mainpage.tsx
+++ src/components/Mainpage.tsx
@@ -1,33 +1,36 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { Figure } from "react-bootstrap";
+import { Figure, Button } from "react-bootstrap";
 import { Competition } from "../shared/types";
 import ImageCard from "./ImageCard/ImageCard";
-import { useFetch } from '../hooks/useFetch';
-import Axios from 'axios';
+import { useFetch } from "../hooks/useFetch";
+import Axios from "axios";
 import "./Mainpage.scss";
 import CustomTable from "./CustomTable";
 
-const abakusLogo = require('../../public/images/abakus_logo_improved.png').default;
+const abakusLogo = require("../../public/images/abakus_logo_improved.png")
+  .default;
 
-const backendUrl = 'http://localhost:3000';
+const backendUrl = "http://localhost:3000";
 
 const competitions = [
   {
     name: "Påskenøtter 2021",
     image: "https://thumbs.dreamstime.com/b/toy-easter-chickne-2102239.jpg",
-    isActive: true
+    isActive: true,
   },
   {
     name: "Julenøtter 2021",
-    image: "https://image.forskning.no/122244.jpg?imageId=122244&width=480&height=274",
-    isActive: true
+    image:
+      "https://image.forskning.no/122244.jpg?imageId=122244&width=480&height=274",
+    isActive: true,
   },
   {
     name: "Ballenøtter 2021",
-    image: "https://lyngstadernaering.no/wp-content/uploads/2014/06/breads_flavored_with_onion_and_garlic_2.jpg",
-    isActive: true
-  }
+    image:
+      "https://lyngstadernaering.no/wp-content/uploads/2014/06/breads_flavored_with_onion_and_garlic_2.jpg",
+    isActive: true,
+  },
 ];
 
 const Mainpage: React.FC = () => {
@@ -40,30 +43,31 @@ const Mainpage: React.FC = () => {
       setCompetitions(response);
     }
     console.log("competitions is: ", competitions);
-  })
+  });
 
   return (
     <div id="mainpage">
-
       <div className="main-header-container">
+        <Button href={`${backendUrl}/auth`}>Logg inn</Button>
         <h1 className="main-heading">Kodenøtter</h1>
-        <img height="50" src={abakusLogo} alt={'abakuslogo'} />
+        <img height="50" src={abakusLogo} alt={"abakuslogo"} />
       </div>
       <div className="competition-container">
-        {competitions.length > 0 ? competitions.map((competition, i: number) => {
-          return <ImageCard
-            key={i}
-            name={competition.name}
-            image={competition.image}
-            isTask={false}
-          // isActive={competition.isActive} 
-          />
-        })
-          :
-          <div>
-            Ingen nøtt å finne.
-          </div>
-          }
+        {competitions.length > 0 ? (
+          competitions.map((competition, i: number) => {
+            return (
+              <ImageCard
+                key={i}
+                name={competition.name}
+                image={competition.image}
+                isTask={false}
+                // isActive={competition.isActive}
+              />
+            );
+          })
+        ) : (
+          <div>Ingen nøtt å finne.</div>
+        )}
       </div>
     </div>
   );
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const useFetch = (url: string): any => {
   const [response, setResponse] = useState<any>(null);
@@ -9,7 +10,11 @@ export const useFetch = (url: string): any => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(url);
+        const token = Cookies.get("auth");
+        const config = token
+          ? { headers: { Authorization: `JWT ${token}` } }
+          : undefined;
+        const res = await axios.get(url, config);
         setResponse(res.data);
       } catch (error) {
         setError(error);
